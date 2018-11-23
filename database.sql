1e6a26bf8e19a6b172f1bc7e0853d4a723365791
Ryan Mundy <ryanmundy34@gmail.com>
2018-11-23 08:19:28 -0600

    set up html wireframe

--- database.sql
+++ database.sql
@@ -2,10 +2,10 @@ CREATE TABLE tasks
 (
     id SERIAL PRIMARY KEY,
     task VARCHAR (250) NOT NULL,
-    completed VARCHAR(250) NOT NULL
+    completed VARCHAR(1) NOT NULL
 );
 
-INSERT INTO tasks(task, completed) VALUES('wash the dishes', 'No');
-INSERT INTO tasks(task, completed)VALUES('take dog for walk', 'No');
-INSERT INTO tasks(task, completed)VALUES('get groceries', 'No');
+INSERT INTO tasks(task, completed) VALUES('wash the dishes', 'N');
+INSERT INTO tasks(task, completed)VALUES('take dog for walk', 'N');
+INSERT INTO tasks(task, completed)VALUES('get groceries', 'N');
 
